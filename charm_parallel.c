7815b43e21454c493d6e1a0bfe14abbdfdc9f324
andreleo02 <andreleo02@gmail.com>
2025-01-11 18:00:15 +0100

    divide dataset between processes

--- charm_parallel.c
+++ charm_parallel.c
@@ -1,10 +1,12 @@
 #include "charm_functions.h"
+#include "utils.h"
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
-    int rank, size = 0;
+    int rank = 0, size = 0;
     MPI_Init(NULL, NULL);
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -12,12 +14,28 @@ int main() {
 
     printf("My rank is %d/%d\n", rank, size);
 
-    // get the dataset
-    // divide dataset ?
+    // get and divide the dataset
+    int num_transactions = 0, partition_size = 0, local_size = 0;
+    Set *transactions = read_sets_from_file_start_end("data/small_transactions.dat", &num_transactions, rank, size, &partition_size, &local_size, true);
+    printf("Process %d has the %d transactions\n", rank, local_size);
+    // for (int i = 0; i < local_size; i++) {
+    //   Set *t = &transactions[i];
+    //   printf("\nItemset (size %d): ", t->size);
+    //   for (int j = 0; j < t->size; j++) {
+    //     printf(" %c ", index_to_char(t->set[j]));
+    //   }
+    // }
+    // printf("\n");
+
+
     // compute first iteration in serial ??? No, i think that should already be parallelized
+    charm(transactions, local_size, 3);
+
     // compute charm_extend() in parallel
+
     // how to gather the results later? MPI_Gather I suppose ...
 
+    free(transactions);
     MPI_Finalize();
 
     return 0;
--- utils.h
+++ utils.h
@@ -39,4 +39,5 @@ void print_closed_itemsets(ITArray C, bool character);
 int char_to_index(char c);
 char index_to_char(int i);
 int count_lines_in_file(const char *filename);
+Set *read_sets_from_file_start_end(const char *filename, int *num_transactions, int rank, int size, int *partition_size, int *local_size, bool characters);
 Set *read_sets_from_file(const char *filename, int *num_transactions, bool characters);
