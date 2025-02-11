cf28d384665fb7890fcaa91b7153700b4ff25a01
andreleo02 <andreleo02@gmail.com>
2025-02-11 18:30:25 +0100

    fixed script

--- simple_qsub_script.sh
+++ simple_qsub_script.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-#PBS -l select=1:ncpus=1
+#PBS -l select=1:ncpus=2:mem=2gb
 
 # define the queue
 #PBS -q short_cpuQ
 
 # set the excecution time
-#PBS -l walltime=00:00:10
+#PBS -l walltime=00:01:00
 
 # set user (optional)
 #PBS -M andrea.leoni-1@unitn.it
 
 module load mpich-3.2
-mpirun.actual -n ./charm_parallel
+module load openmpi-4.0.4
+module load glibc-2.32
+mpirun.actual -n 2 $(pwd)/Charm/charm_parallel_cluster
