bcaea425c6e1be4fcfa62f6b99abb12559924fa7
Leoni <andrea.leoni-1@hpc-head-n1.unitn.it>
2025-02-12 11:25:56 +0100

    set simple script

--- simple_qsub_script.sh
+++ simple_qsub_script.sh
@@ -6,12 +6,16 @@
 #PBS -q short_cpuQ
 
 # set the excecution time
-#PBS -l walltime=00:01:00
+#PBS -l walltime=00:00:10
 
 # set user (optional)
 #PBS -M andrea.leoni-1@unitn.it
 
+# Set OpenMP thread count
+export OMP_NUM_THREADS=2
+
 module load mpich-3.2
 module load openmpi-4.0.4
 module load glibc-2.32
-mpirun.actual -n 2 $(pwd)/Charm/charm_parallel_cluster
+
+mpiexec -n 2 --verbose $(pwd)/Charm/out/charm_parallel_cluster
