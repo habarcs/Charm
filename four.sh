942036df90295a36f610354c06d929f0e6eb6321
andreleo02 <andreleo02@gmail.com>
2025-04-23 11:55:42 +0200

    added separate sh files

---
+++ four.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#PBS -l select=2:ncpus=2:mem=2gb
+
+# define the queue
+#PBS -q short_cpuQ
+
+# set the excecution time
+#PBS -l walltime=00:10:00
+
+# set user (optional)
+#PBS -M andrea.leoni-1@unitn.it
+
+module load mpich-3.2
+
+export CHARM_OPENMP=0
+export DATA_PATH=/home/andrea.leoni-1/Charm/data
+export DATA_FILE=retail.dat
+export DATA_CHARACTERS=0
+export MIN_SUPPORT=10000
+export MAX_TRANSACTIONS=20000
+
+mpiexec -n 4 $(pwd)/Charm/out/charm_parallel
