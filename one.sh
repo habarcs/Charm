1a254e4fee6b148f7ec0c80cf254ec59d680cacf
andreleo02 <andreleo02@gmail.com>
2025-04-23 12:11:27 +0200

    changed number of samples

--- one.sh
+++ one.sh
@@ -15,9 +15,9 @@ module load mpich-3.2
 
 export CHARM_OPENMP=0
 export DATA_PATH=/home/andrea.leoni-1/Charm/data
-export DATA_FILE=retail.dat
+export DATA_FILE=synthetic_retail.dat
 export DATA_CHARACTERS=0
 export MIN_SUPPORT=10000
-export MAX_TRANSACTIONS=20000
+export MAX_TRANSACTIONS=100000
 
 mpiexec -n 1 $(pwd)/Charm/out/charm_parallel
